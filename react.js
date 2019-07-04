866f3961f3edf735dcd2f4f48c2f4658f887aa18
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2019-07-03 21:05:53 -0400

    Getting Started with React Redux

--- react.js
+++ react.js
@@ -1,32 +1,13 @@
-class MyComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        display: true
-      }
-      this.toggleDisplay = this.toggleDisplay.bind(this);
-    }
-    toggleDisplay() {
-      this.setState({
-        display: !this.state.display
-      });
-    }
-    render() {
-      // change code below this line
-  if (this.state.display) {
-  
-      return (
-         <div>
-           <button onClick={this.toggleDisplay}>Toggle Display</button>
-           <h1>Displayed!</h1>
-         </div>
-      );}
-      else {
-        return (
-          <div>
-          <button onClick={this.toggleDisplay}>Toggle Display</button>
-          Displayed!
-          </div>
-        );}
-    }
-  };
+class DisplayMessages extends React.Component {
+  // change code below this line
+constructor(props){
+    super(props);
+
+    this.state = {input: "", messages: []}
+}
+
+  // change code above this line
+  render() {
+    return <div />
+  }
+};
