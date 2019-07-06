c8ca6e8af9b0d0cc41b7fa52d351e5f6e85e7ffc
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2019-07-05 21:03:18 -0400

    Redux: Send Action Data to the Store

--- react.js
+++ react.js
@@ -1,13 +1,27 @@
-class DisplayMessages extends React.Component {
-  // change code below this line
-constructor(props){
-    super(props);
+const ADD_NOTE = 'ADD_NOTE';
 
-    this.state = {input: "", messages: []}
-}
-
-  // change code above this line
-  render() {
-    return <div />
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+    // change code below this line
+case ADD_NOTE:
+return state = action.text
+    // change code above this line
+    default:
+      return state;
   }
 };
+
+const addNoteText = (note) => {
+  // change code below this line
+return {
+  type: ADD_NOTE,
+  text: note
+}
+  // change code above this line
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
